cab48e2884ed1543b2b89862b3cd7f60c444190d
Thibaut Lienart <tlienart@me.com>
2018-08-18 23:03:26 +0200

    small modifs

--- run_judoc.jl
+++ run_judoc.jl
@@ -1,20 +1,25 @@
 using JuDoc
 
-# this is needed for a "clean exit" when running from outside REPL.
-ccall(:jl_exit_on_sigint, Void, (Cint,), 0)
+const SINGLE_PASS = false
+const FOLDER_PATH = "/Users/tlienart/Desktop/tlienart.github.io/"
 
-# curdir = normpath(joinpath(@__DIR__, "/"))
-# FOLDER_PATH = (typeof(curdir)==Void) ? pwd() : curdir
+if SINGLE_PASS
+    JuDoc.judoc()
+else
+    # this is needed for a "clean exit" when running from outside REPL.
+    ccall(:jl_exit_on_sigint, Nothing, (Cint,), 0)
 
-FOLDER_PATH = "/Users/tlienart/Desktop/tlienart.github.io/"
+    # curdir = normpath(joinpath(@__DIR__, "/"))
+    # FOLDER_PATH = (typeof(curdir)==Void) ? pwd() : curdir
 
-# NOTE: works with bash, would have to be tested with other shells, we don't
-# care at this point, when other users, we'll see
-# NOTE: add a message, should go `localhost:8000`, also make this a function
-# wrapping around the JuDoc call with single_pass = false or something.
-run(`bash -c "browser-sync start -s -f $FOLDER_PATH --no-notify --logLevel silent --port 8000 --no-open &"`)
 
-# this is blocking, when interrupted, it also kills the background process.
-judoc(single_pass=false)
 
-# all done.
+    # NOTE: works with bash, would have to be tested with other shells, we don't
+    # care at this point, when other users, we'll see
+    # NOTE: add a message, should go `localhost:8000`, also make this a function
+    # wrapping around the JuDoc call with single_pass = false or something.
+    run(`bash -c "browser-sync start -s -f $FOLDER_PATH --no-notify --logLevel silent --port 8000 --no-open &"`)
+
+    # this is blocking, when interrupted, it also kills the background process.
+    JuDoc.judoc(single_pass=false)
+end
